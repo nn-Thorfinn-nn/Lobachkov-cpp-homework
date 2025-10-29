c69a730a2344d22f5ee0e55512b281b7e5d3a778
nn-Thorfinn-nn <lochkabov2@gmail.com>
2025-10-29 17:04:36 +0500

    homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,10 +1,11 @@
 #include <iostream> //подключение библиотеки с вводом-выводом (cin, cout)
 #include <string> //для удобства
-using namespace std; 
+using namespace std;
 int main() //начало выполнения
-{ 
-      string userName; 
-    cin >> userName; //ввод имени
-    cout << "Здравствуйте, " + userName + "!"; //вывод приветственного сообщения
-    return 0; //завершение программы 
+{
+    string userName; 
+    cout << "Введите ваше имя:"; //Запрос имени пользователя
+    getline(cin, userName); //считывание строки с именем
+    cout << "Здравствуйте, " << userName << endl; //вывод приветственного сообщения
+    return 0; //завершение программы
 }
